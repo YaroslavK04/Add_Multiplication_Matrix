136c7f2981b92ccd1ea6ed4e2cc77d6535362a50
YaroslavK04 <114668684+YaroslavK04@users.noreply.github.com>
2024-10-16 18:40:49 +0300

    добавили умножение и сложение матриц в СОК, преобразование из СОК в ПСС

--- Source.cpp
+++ Source.cpp
@@ -4,33 +4,60 @@
 #include<fstream>
 #include<sstream>
 #include <string>
+#include<cmath>
 
 using namespace std;
 
 vector<vector<int>> prime;// вектор для хранения базисов 
 
+
+uint64_t modInverse(uint64_t Mi, uint64_t basis) {
+    uint64_t i = 0;
+    uint64_t j;
+    while (((Mi * i) % basis) != 1) {
+        j = ((Mi * i) % basis);
+        i++;
+       
+    }
+    j = ((Mi * i) % basis);
+    return i;
+ 
+}
+
+
+
+
 class PSS {
 private:
-    vector<vector<uint16_t>> matrixPSS;
- 
+    vector<vector<uint64_t>> matrixPSS;
+
 public:
     void convertSOK_PSS(vector<vector<vector<int>>> matrixInSOK, vector<int> basis) {
-        uint64_t multBasis=1;
+        uint64_t multBasis = 1;
+        uint64_t Mi;
+
         // Инициализация matrixPSS с нужным размером
-        matrixPSS.assign(matrixInSOK.size(), vector<uint16_t>(matrixInSOK[0].size(), 0));
+        matrixPSS.assign(matrixInSOK.size(), vector<uint64_t>(matrixInSOK[0].size(), 0));
 
         // Вычисление произведения оснований
         for (auto bas : basis) {
             multBasis *= bas;
         }
-
         vector<uint64_t> convertBasis(basis.size());
-        for (int i = 0; i < convertBasis.size(); i++) {
-            convertBasis[i] = multBasis / basis[i];
-            convertBasis[i] *= convertBasis[i] % basis[i];
+        try {
+
+            for (int i = 0; i < convertBasis.size(); i++) {
+                Mi = multBasis / basis[i];
+                convertBasis[i] = Mi;
+                convertBasis[i] *= modInverse(Mi, basis[i]);
+            }
+        }
+        catch (const runtime_error& e) {
+            cout << e.what() << endl;
         }
 
-        uint64_t convPSS ;
+        uint64_t convPSS;
+     
 
         // Преобразование матрицы
         for (int row = 0; row < matrixInSOK.size(); row++) {
@@ -39,8 +66,11 @@ public:
                 for (int i = 0; i < basis.size(); i++) {
                     convPSS += matrixInSOK[row][col][i] * convertBasis[i];
                 }
+               
                 matrixPSS[row][col] = convPSS % multBasis;
+              
             }
+       
         }
         printMatrixPSS();
     }
@@ -57,14 +87,15 @@ public:
 
 
 class SOK {
-private:
+
+
+public:
     vector<vector<vector<int>>> matrixSOK;
     vector<int> basis;
-public:
     void convertPSS_SOK(vector<vector<uint64_t>> z, int maxValue) {
          int mult = 1;
 
-          bool br = false;
+         bool br = false;
           for (auto row : prime) {
               for (auto val : row) {
                   mult *= val;
@@ -83,7 +114,7 @@ public:
               }
           }
 
-       // basis = prime[0];
+     // basis = prime[0];
 
         cout << "\n\n";
 
@@ -112,24 +143,23 @@ public:
             cout << endl;
         }
 
-        cout << "\n\n";
+        cout << "\n";
 
         PSS kk;
         kk.convertSOK_PSS(matrixSOK, basis);
 
-        cout << "\n\n";
+        cout << "\n";
     }
 
-    vector<vector<vector<int>>> ReturnSOK() { return matrixSOK; }
-    vector<int>  ReturnBasis() { return basis; }
+    
 };
 class MatrixOperation {
-private:
+public:
     vector<vector<uint64_t>> matrix;
     SOK matrixInSok;
-public:
+
     // Метод для создания квадратной матрицы с рандомными значениями
-    MatrixOperation(uint64_t min, uint64_t max, int size) {
+    void GeneraitMatrix(uint64_t min, uint64_t max, int size) {
         // Создание матрицы
         matrix.assign(size, vector<uint64_t>(size));
         // Заполнение матрицы случайными значениями
@@ -150,7 +180,7 @@ public:
 
 
         matrixInSok.convertPSS_SOK(matrix, max);
-        cout << endl;
+      
     }
     // Метод для cложения матриц
     vector<vector<uint64_t>> additionMatrix(vector<vector<uint64_t>>operator1, vector<vector<uint64_t>>operator2, int size) {
@@ -176,8 +206,80 @@ public:
             }
             cout << endl;
         }
+
+        cout << endl;
         return matrixMultiplication;
     }
+
+    // Метод для cложения матриц
+    vector<vector<vector<int>>> additionMatrixInSOK(vector<vector<vector<int>>> matriInSok2, int size) {
+        int del;
+        vector<vector<vector<int>>> AddMatrixSok(matriInSok2.size(), vector<vector<int>>(matriInSok2.size(), vector<int>(matriInSok2[0][0].size(), 0)));
+        for (int row = 0; row < matriInSok2.size(); row++) {
+            for (int col = 0; col < matriInSok2[row].size(); col++) {
+                for (int i = 0; i < matriInSok2[row][col].size(); i++) {
+                    AddMatrixSok[row][col][i] = matriInSok2[row][col][i] + matrixInSok.matrixSOK[row][col][i];
+                    if (AddMatrixSok[row][col][i] >= matrixInSok.basis[i]) {
+                        del = AddMatrixSok[row][col][i] / matrixInSok.basis[i];
+                        AddMatrixSok[row][col][i] -= del * matrixInSok.basis[i];
+                    }
+                }
+            }
+        }
+        for (const auto row : AddMatrixSok) {
+            for (const auto col : row) {
+                for (const auto i : col) {
+                    cout << ((i / 10) == 0 ? " " : "") << i << " ";
+                }
+                cout << "   ";
+            }
+            cout << endl;
+        }
+
+        PSS psss;
+        psss.convertSOK_PSS(AddMatrixSok, prime[0]);
+
+        return AddMatrixSok;
+    }
+
+    vector<vector<vector<int>>> MultMatrixInSOK(vector<vector<vector<int>>> matriInSok2, int size) {
+        vector<vector<vector<int>>> MultMatrixSok(matriInSok2.size(), vector<vector<int>>(matriInSok2.size(), vector<int>(matriInSok2[0][0].size(), 0)));
+        
+        int del;
+        for (int row = 0; row < matriInSok2.size(); row++) {
+            for (int col = 0; col < matriInSok2[row].size(); col++) {
+                for (int k = 0; k < matriInSok2[row].size(); k++) {
+                    for (int prime = 0; prime < MultMatrixSok[row][col].size(); prime++) {
+                        MultMatrixSok[row][col][prime] += matriInSok2[k][col][prime] * matrixInSok.matrixSOK[row][k][prime]; 
+                    }
+                }
+                for (int prime = 0; prime < MultMatrixSok[row][col].size(); prime++) {
+                    if (MultMatrixSok[row][col][prime] >= matrixInSok.basis[prime]) {
+                        del = MultMatrixSok[row][col][prime] / matrixInSok.basis[prime];
+                        MultMatrixSok[row][col][prime] -= del * matrixInSok.basis[prime];
+                        //MultMatrixSok[row][col][prime] -= matrixInSok.basis[prime];
+                    }
+                }
+            }
+        }
+
+        for (const auto row : MultMatrixSok) {
+            for (const auto col : row) {
+                for (const auto i : col) {
+                    cout << ((i / 10) == 0 ? " " : "") << i << " ";
+                }
+                cout << "   ";
+            }
+            cout << endl;
+        }
+
+        PSS psss;
+        psss.convertSOK_PSS(MultMatrixSok, prime[0]);
+
+        return MultMatrixSok;
+    }
+
+
 };
 
 int main() {
@@ -186,11 +288,9 @@ int main() {
     SOK SokMatrix1;
     SOK SokMatrix2;
     SOK SokMatrix3;
-
    
-
-    //vector<vector<uint64_t>> matrix1;
-    vector<vector<uint64_t>> matrix2;
+    MatrixOperation matrix1;
+    MatrixOperation matrix2;
 
     vector<vector<uint64_t>> ResultAdd;
     vector<vector<uint64_t>> ResultMultiplication;
@@ -218,7 +318,7 @@ int main() {
 
     while (choose != 4)
     {
-        cout << "Выберите действие:\n1)Сгенерировать матрицы\n2)Сложить матрицы\n3)Перемножить матрицы\n4)Закончить выполнение программы\n";
+        cout << "\nВыберите действие:\n1)Сгенерировать матрицы\n2)Сложить матрицы\n3)Перемножить матрицы\n4)Закончить выполнение программы\n";
         cin >> choose;
         while (choose < 1 || choose > 4) {
             cout << "Неверное значение. Повторите выбор.";
@@ -239,27 +339,34 @@ int main() {
             cout << "max: ";
             cin >> RandomRangeMax;
 
-            cout << "Матрица 1\n"<<endl;
-            MatrixOperation matrix1(RandomRangeMin, RandomRangeMax, size);
+            cout << "Матрица 1"<<endl;
+            matrix1.GeneraitMatrix(RandomRangeMin, RandomRangeMax, size);
+            
           
             //Mатрица 2
-            cout << "Матрица 2\n"<<endl;
-            MatrixOperation matrix2(RandomRangeMin, RandomRangeMax, size);
+            cout << "Матрица 2"<<endl;
+            matrix2.GeneraitMatrix(RandomRangeMin, RandomRangeMax, size);
 
-         /*   cout << "\n СОК первой матрицы";
-            SokMatrix1.convertPPS_SOK(matrix1, RandomRangeMax);
-            cout << "\n СОК второй матрицы";
-            SokMatrix2.convertPPS_SOK(matrix2, RandomRangeMax);*/
+        
 
             break;
         }
         case(2): {
-           /* ResultAdd = generator.additionMatrix(matrix1, matrix2, size);*/
+            ResultAdd = matrix1.additionMatrix(matrix1.matrix, matrix2.matrix, size);
+            matrix1.additionMatrixInSOK(matrix2.matrixInSok.matrixSOK, size);
             break;
         }
         case(3):
-            /*ResultMultiplication = generator.multiplicationMatrix(matrix1, matrix2, size);
-            SokMatrix2.convertPPS_SOK(ResultMultiplication, RandomRangeMax);*/
+            ResultMultiplication = matrix1.multiplicationMatrix(matrix1.matrix,matrix2.matrix, size);
+            /*SokMatrix2.convertPPS_SOK(ResultMultiplication, RandomRangeMax);*/
+            matrix1.MultMatrixInSOK( matrix2.matrixInSok.matrixSOK, size);
+
+           /* cout << endl;
+
+            SOK yuyu;
+            yuyu.convertPSS_SOK(ResultMultiplication, 9);*/
+
+            
 
             break;
         }
